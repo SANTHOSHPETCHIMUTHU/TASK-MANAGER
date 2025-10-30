aaf01e306f0287fd4f16b2db1518d6f8fec693bb
SANTHOSH KUMAR PETCHIMUTHU <149028859+SANTHOSHPETCHIMUTHU@users.noreply.github.com>
2025-10-30 22:49:50 +0530

    Update EmployeeServiceApplicationTests.java

--- backend/employee-service/src/test/java/com/qcomit/employee_service/EmployeeServiceApplicationTests.java
+++ backend/employee-service/src/test/java/com/qcomit/employee_service/EmployeeServiceApplicationTests.java
@@ -3,11 +3,14 @@ package com.qcomit.employee_service;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(properties = {
+    "spring.datasource.url=jdbc:h2:mem:testdb",
+    "spring.datasource.driverClassName=org.h2.Driver",
+    "spring.jpa.hibernate.ddl-auto=update"
+})
 class EmployeeServiceApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
-
+    @Test
+    void contextLoads() {
+    }
 }
