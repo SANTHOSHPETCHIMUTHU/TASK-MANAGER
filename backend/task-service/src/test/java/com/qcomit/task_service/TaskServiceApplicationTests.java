893dbb56749137b27002344e3273631933eab063
SANTHOSH KUMAR PETCHIMUTHU <149028859+SANTHOSHPETCHIMUTHU@users.noreply.github.com>
2025-10-30 22:51:37 +0530

    Add H2 database properties to test configuration

--- backend/task-service/src/test/java/com/qcomit/task_service/TaskServiceApplicationTests.java
+++ backend/task-service/src/test/java/com/qcomit/task_service/TaskServiceApplicationTests.java
@@ -3,11 +3,14 @@ package com.qcomit.task_service;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(properties = {
+    "spring.datasource.url=jdbc:h2:mem:testdb",
+    "spring.datasource.driverClassName=org.h2.Driver",
+    "spring.jpa.hibernate.ddl-auto=create-drop"
+})
 class TaskServiceApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
-
+    @Test
+    void contextLoads() {
+    }
 }
